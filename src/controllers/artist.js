125f3caae59440537ce0fb41f18263556a130976
James Goodman <jamesgoodman206@gmail.com>
2022-12-18 22:11:00 +0000

    completed Reading Single Artists, both tests passing

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -18,3 +18,27 @@ exports.createArtist = async (req, res) => {
       res.status(500).json(err.message)
     }
   }
+
+  exports.getAllArtists = async (_, res) => {
+    try {
+      const { rows } = await db.query('SELECT * FROM Artists')
+      res.status(200).json(rows)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+
+  exports.getArtistById = async (req, res) => {
+    try {
+      const { id } = req.params
+      const { rows: [ artist ] } = await db.query('SELECT * FROM Artists WHERE id = $1', [ id ])
+  
+      if (!artist) {
+        return res.status(404).json({ message: `artist ${id} does not exist` })
+      }
+  
+      res.status(200).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -6,4 +6,8 @@ const router = express.Router();
 
 router.post('/', artistController.createArtist);
 
+router.get('/', artistController.getAllArtists);
+
+router.get('/:id', artistController.getArtistById);
+
 module.exports = router;
