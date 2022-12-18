45417d030bc79a7b44461d792d1d89d0715c088a
James Goodman <jamesgoodman206@gmail.com>
2022-12-18 16:28:12 +0000

    trying to get Creating Artists working

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,8 +1,18 @@
 // exports.create = (req, res) => {
 //     res.sendStatus(201);
 // }
-const create = (req, res) => {
-    res.sendStatus(201);
-}
+// const create = (req, res) => {
+//     res.sendStatus(201);
+// }
 
-module.exports = { create }
+// module.exports = { create }
+exports.createArtist = async (req, res) => {
+    const { name, genre } = req.body
+  
+    try {
+      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+      res.status(201).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
